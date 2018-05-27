7ca1017e326f41befc0c6b3e33ebda7c1dd860f3
Chris de Graaf <chrisadegraaf@gmail.com>
2018-05-27 13:42:09 -0500

    Add request/response handling, generate response map

--- client.go
+++ client.go
@@ -3,21 +3,27 @@ package obsws
 import (
 	"crypto/sha256"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/mitchellh/mapstructure"
 	"github.com/pkg/errors"
 )
 
 // Client is the interface to obs-websocket.
 // Client{Host: "localhost", Port: 4444} will probably work if you haven't configured OBS.
 type Client struct {
-	Host     string // Host (probably "localhost").
-	Port     int    // Port (OBS default is 4444).
-	Password string // Password (OBS default is "").
-	conn     *websocket.Conn
-	id       int
+	Host       string // Host (probably "localhost").
+	Port       int    // Port (OBS default is 4444).
+	Password   string // Password (OBS default is "").
+	conn       *websocket.Conn
+	id         int
+	respQ      []response
+	timeout    int
+	timeoutSet bool
 }
 
 // Connect opens a WebSocket connection and authenticates if necessary.
@@ -68,6 +74,80 @@ func (c *Client) Connect() error {
 // Disconnect closes the WebSocket connection.
 func (c *Client) Disconnect() error {
 	return c.conn.Close()
+}
+
+// SetTimeout sets a timeout in seconds for receiving responses.
+// A value of 0 indicates that responses must be received instantly.
+func (c *Client) SetTimeout(seconds int) {
+	c.timeoutSet = true
+	c.timeout = seconds
+}
+
+// DisableTimeout disables the response timeout.
+func (c *Client) DisableTimeout() {
+	c.timeoutSet = false
+}
+
+// SendRequest sends a request to the WebSocket server.
+func (c *Client) SendRequest(req request) (chan response, error) {
+	future := make(chan response)
+	if err := c.conn.WriteJSON(req); err != nil {
+		return nil, errors.Wrapf(err, "write %s", req.Type())
+	}
+	go func() { future <- c.waitResponse(req) }()
+	return future, nil
+}
+
+func (c *Client) waitResponse(req request) response {
+	start := time.Now()
+	for {
+		for i, resp := range c.respQ {
+			if resp.(message).ID() == req.(message).ID() {
+				c.respQ = append(c.respQ[:i], c.respQ[i+1:]...)
+				return resp
+			}
+		}
+
+		resp, err := c.receiveResponse(req)
+		if err != nil {
+			logger.Warning(err)
+		}
+
+		if resp.(message).ID() == req.(message).ID() {
+			return resp
+		}
+		c.respQ = append(c.respQ, resp)
+
+		if c.timeoutSet && time.Since(start).Seconds() > float64(c.timeout) {
+			logger.Infof("request %s (%s) timed out", req.(message).ID(), req.Type())
+			return nil
+		}
+	}
+}
+
+func (c *Client) receiveResponse(req request) (response, error) {
+	m := make(map[string]interface{})
+
+	_, bytes, err := c.conn.ReadMessage()
+	if err != nil {
+		return nil, errors.Wrap(err, "read from WS")
+	}
+
+	if err = json.Unmarshal(bytes, &m); err != nil {
+		logger.Warningf("unmarshalling JSON -> map failed: %v", err)
+		return nil, errors.Wrap(err, "unmarshalling JSON -> map")
+	}
+
+	resp, ok := respMap[req.Type()]
+	if !ok {
+		return nil, errors.Errorf("unknown request type '%s'", req.Type())
+	}
+
+	if err = mapstructure.Decode(m, &resp); err != nil {
+		return nil, errors.Wrapf(err, "unmarshalling map -> %T", resp)
+	}
+
+	return resp, nil
 }
 
 func connectWS(host string, port int) (*websocket.Conn, error) {
