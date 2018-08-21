4694eaacef5a06848c2de7363608c233af11d04d
Chris de Graaf <chrisadegraaf@gmail.com>
2018-08-21 00:35:40 -0500

    Return error when not connected instead of segfaulting

--- client_requests.go
+++ client_requests.go
@@ -7,10 +7,16 @@ import (
 
 const interval = time.Millisecond * 50
 
-var ErrReceiveTimeout = errors.New("receive timed out")
+var (
+	ErrNotConnected   = errors.New("not connected")
+	ErrReceiveTimeout = errors.New("receive timed out")
+)
 
 // sendRequest sends a request to the WebSocket server.
 func (c *Client) sendRequest(req Request) (chan map[string]interface{}, error) {
+	if !c.active {
+		return nil, ErrNotConnected
+	}
 	future := make(chan map[string]interface{})
 	if err := c.conn.WriteJSON(req); err != nil {
 		return nil, err
